67ba363ee8c4225a2bce4f024f7adb5c9d0614ed
Josh <jephillips@cohesionforce.com>
2015-03-18 13:27:28 -0500

    Populated MainActivity with boilerplate

--- app/src/main/java/com/example/jephillips/ereader/MainActivity.java
+++ app/src/main/java/com/example/jephillips/ereader/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.jephillips.ereader;
 
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return (super.onCreateOptionsMenu(menu));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                return (true);
+            case R.id.action_settings:
+                return (true);
+
+        }
+        return (super.onOptionsItemSelected(item));
     }
 }
