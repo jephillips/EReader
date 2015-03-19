c7874d4ca3c9b669d52bec3fafadd6f9930f990b
Josh <jephillips@cohesionforce.com>
2015-03-19 13:23:33 -0500

    Added ViewPager

--- app/src/main/java/com/example/jephillips/ereader/MainActivity.java
+++ app/src/main/java/com/example/jephillips/ereader/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.jephillips.ereader;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
 
 public class MainActivity extends Activity {
+
+    private ViewPager pager = null;
+    private ContentsAdapter adapter = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.pager);
+        adapter = new ContentsAdapter(this);
+        pager.setAdapter(adapter);
+        findViewById((R.id.progressBar)).setVisibility(View.GONE);
+        pager.setVisibility(View.VISIBLE);
     }
 
     @Override
