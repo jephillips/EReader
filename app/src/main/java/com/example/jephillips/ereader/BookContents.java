8e316394f76ffeb6ace1c86c13cd7bfc61e10b94
Josh <jephillips@cohesionforce.com>
2015-03-24 14:52:18 -0500

    Added ModelFragment and Book related classes/assets

---
+++ app/src/main/java/com/example/jephillips/ereader/BookContents.java
@@ -0,0 +1,30 @@
+package com.example.jephillips.ereader;
+
+/**
+ * Created by jephillips on 3/24/15.
+ */
+
+import java.util.List;
+
+public class BookContents {
+
+    String title;
+    List<BookContents.Chapter> chapters;
+
+    int getChapterCount() {
+        return chapters.size();
+    }
+
+    String getChapterFile(int position) {
+        return chapters.get(position).file;
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    static class Chapter {
+        String file;
+        String title;
+    }
+}
